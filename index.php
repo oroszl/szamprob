036120c4ff7ef749ff9600a468b99c1e46ec6b89
oroszl <oroszlanyl@gmail.com>
2016-10-22 21:43:55 +0200

    update  webpage

--- index.php
+++ index.php
@@ -91,9 +91,9 @@ A ZH-n megjelenni kötelező, a jegyszerzés szükséges feltétele!
 <td style="text-align:center; border: 1px solid #dddddd "><a href='static_notebooks/Package04/mintapelda04.html'>Mintapelda-04</a><br>  
                                                           <a href='notebooks/Package04/pakk.tgz' download>(forrás)</a></td>
 <td style="text-align:center; border: 1px solid #dddddd "><a href='static_notebooks/Package05/mintapelda05.html'>Mintapelda-05</a><br>  
-                                                          <a href='notebooks/Package05/mintapelda05.ipynb' download>(forrás)</a><a href=../../notebooks/Package05/data/' >(adatok)</a></td>
+                                                          <a href='notebooks/Package05/mintapelda05.ipynb' download>(forrás)</a><a href=../notebooks/Package05/data/' >(adatok)</a></td>
 <td style="text-align:center; border: 1px solid #dddddd "><a href='static_notebooks/Package06/mintapelda06.html'>Mintapelda-06</a><br>  
-                                                          <a href='notebooks/Package06/mintapelda06.ipynb' download>(forrás)</a><a href=../../notebooks/Package06/data/' >(adatok)</a></td>
+                                                          <a href='notebooks/Package06/mintapelda06.ipynb' download>(forrás)</a><a href=../notebooks/Package06/data/' >(adatok)</a></td>
 
     
 </tr>
